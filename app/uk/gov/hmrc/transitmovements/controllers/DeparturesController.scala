5191202ea449da16632e0971a3f9224336389cca
liandra-giacomine <liandra.giacomine@opencastsoftware.com>
2022-06-08 11:04:17 +0100

    Moved TemporaryFile functions from controller to its own trait

--- app/uk/gov/hmrc/transitmovements/controllers/DeparturesController.scala
+++ app/uk/gov/hmrc/transitmovements/controllers/DeparturesController.scala
@@ -19,20 +19,13 @@ package uk.gov.hmrc.transitmovements.controllers
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 import akka.stream.Materializer
 import akka.stream.scaladsl.FileIO
-import akka.stream.scaladsl.Sink
-import akka.stream.scaladsl.Source
-import akka.util.ByteString
-import cats.data.EitherT
 import play.api.Logging
 import play.api.mvc.ControllerComponents
-import play.api.mvc.Request
 import play.api.mvc.Result
 import play.api.libs.json.Json
-import play.api.libs.Files
 import play.api.libs.Files.TemporaryFileCreator
 import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.play.http.HeaderCarrierConverter
-import uk.gov.hmrc.transitmovements.controllers.errors.PresentationError
 import uk.gov.hmrc.transitmovements.controllers.errors.ConvertError
 import uk.gov.hmrc.transitmovements.controllers.stream.StreamingParsers
 import uk.gov.hmrc.transitmovements.models.EORINumber
@@ -41,56 +34,35 @@ import uk.gov.hmrc.transitmovements.services.DeparturesXmlParsingService
 
 import javax.inject.Inject
 import javax.inject.Singleton
-import scala.concurrent.Future
-import scala.util.Try
 
 @Singleton
 class DeparturesController @Inject() (
   cc: ControllerComponents,
   departuresService: DeparturesService,
   xmlParsingService: DeparturesXmlParsingService,
-  temporaryFileCreator: TemporaryFileCreator
+  val temporaryFileCreator: TemporaryFileCreator
 )(implicit
   val materializer: Materializer
 ) extends BackendController(cc)
     with Logging
     with StreamingParsers
-    with ConvertError {
-
-  private def deleteFile[A](temporaryFile: Files.TemporaryFile)(identity: A): A = {
-    temporaryFile.delete()
-    identity
-  }
-
-  def withTemporaryFile[A](
-    onSucceed: (Files.TemporaryFile, Source[ByteString, _]) => EitherT[Future, PresentationError, A]
-  )(implicit request: Request[Source[ByteString, _]]): EitherT[Future, PresentationError, A] =
-    EitherT(Future.successful(Try(temporaryFileCreator.create()).toEither))
-      .leftMap {
-        thr =>
-          request.body.runWith(Sink.ignore)
-          PresentationError.internalServiceError(cause = Some(thr))
-      }
-      .flatMap {
-        temporaryFile =>
-          val source = request.body.alsoTo(FileIO.toPath(temporaryFile.path))
-          onSucceed(temporaryFile, source).bimap(deleteFile(temporaryFile), deleteFile(temporaryFile))
-      }
+    with ConvertError
+    with TemporaryFiles {
 
   def post(eori: EORINumber) = Action.async(streamFromMemory) {
     implicit request =>
       implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequest(request)
       withTemporaryFile {
         (temporaryFile, source) =>
-          for {
+          (for {
             declarationData <- xmlParsingService.extractDeclarationData(source).asPresentation
             fileSource = FileIO.fromPath(temporaryFile)
             declarationResponse <- departuresService.create(eori, declarationData, fileSource).asPresentation
-          } yield declarationResponse
-      }.fold[Result](
-        baseError => Status(baseError.code.statusCode)(Json.toJson(baseError)),
-        response => Ok(Json.toJson(response))
-      )
+          } yield declarationResponse).fold[Result](
+            baseError => Status(baseError.code.statusCode)(Json.toJson(baseError)),
+            response => Ok(Json.toJson(response))
+          )
+      }.toResult
   }
 
 }
