a8a53d3fb8060146ee3a6646a7e509bd1df995da
liandra-giacomine <liandra11@hotmail.com>
2023-02-13 16:20:49 +0000

    Made subtypes of sealed types final

--- app/uk/gov/hmrc/transitmovements/models/MessageType.scala
+++ app/uk/gov/hmrc/transitmovements/models/MessageType.scala
@@ -69,21 +69,21 @@ object MessageType {
   // *******************
 
   /** E_DEC_AMD (IE013) */
-  case object DeclarationAmendment extends DepartureRequestMessageType("IE013", "CC013C", "/xsd/CC013C.xsd")
+  final case object DeclarationAmendment extends DepartureRequestMessageType("IE013", "CC013C", "/xsd/CC013C.xsd")
 
   /** E_DEC_INV (IE014) */
-  case object DeclarationInvalidation extends DepartureRequestMessageType("IE014", "CC014C", "/xsd/CC014C.xsd")
+  final case object DeclarationInvalidation extends DepartureRequestMessageType("IE014", "CC014C", "/xsd/CC014C.xsd")
 
   /** E_DEC_DAT (IE015) */
-  case object DeclarationData extends DepartureRequestMessageType("IE015", "CC015C", "/xsd/CC015C.xsd")
+  final case object DeclarationData extends DepartureRequestMessageType("IE015", "CC015C", "/xsd/CC015C.xsd")
 
   /** E_REQ_REL (IE054) */
-  case object RequestOfRelease extends DepartureRequestMessageType("IE054", "CC054C", "/xsd/CC054C.xsd")
+  final case object RequestOfRelease extends DepartureRequestMessageType("IE054", "CC054C", "/xsd/CC054C.xsd")
 
   /** E_PRE_NOT (IE170) */
-  case object PresentationNotification extends DepartureRequestMessageType("IE170", "CC170C", "/xsd/CC170C.xsd")
+  final case object PresentationNotification extends DepartureRequestMessageType("IE170", "CC170C", "/xsd/CC170C.xsd")
 
-  case object InformationAboutNonArrivedMovement extends DepartureRequestMessageType("IE141", "CC141C", "/xsd/CC141C.xsd")
+  final case object InformationAboutNonArrivedMovement extends DepartureRequestMessageType("IE141", "CC141C", "/xsd/CC141C.xsd")
 
   val departureRequestValues = Set(
     DeclarationAmendment,
@@ -99,48 +99,48 @@ object MessageType {
   // ********************
 
   /** E_AMD_ACC (IE004) */
-  case object AmendmentAcceptance extends DepartureResponseMessageType("IE004", "CC004C", "/xsd/CC004C.xsd")
+  final case object AmendmentAcceptance extends DepartureResponseMessageType("IE004", "CC004C", "/xsd/CC004C.xsd")
 
   /** E_DEP_REJ (IE056) */
-  case object DepartureOfficeRejection extends DepartureResponseMessageType("IE056", "CC056C", "/xsd/CC056C.xsd")
+  final case object DepartureOfficeRejection extends DepartureResponseMessageType("IE056", "CC056C", "/xsd/CC056C.xsd")
 
   /** E_INV_DEC (IE009) */
-  case object InvalidationDecision extends DepartureResponseMessageType("IE009", "CC009C", "/xsd/CC009C.xsd")
+  final case object InvalidationDecision extends DepartureResponseMessageType("IE009", "CC009C", "/xsd/CC009C.xsd")
 
   /** E_GUA_INV (IE055) */
-  case object GuaranteeInvalid extends DepartureResponseMessageType("IE055", "CC055C", "/xsd/CC055C.xsd")
+  final case object GuaranteeInvalid extends DepartureResponseMessageType("IE055", "CC055C", "/xsd/CC055C.xsd")
 
   /** E_DIS_SND (IE019) */
-  case object Discrepancies extends DepartureResponseMessageType("IE019", "CC019C", "/xsd/CC019C.xsd")
+  final case object Discrepancies extends DepartureResponseMessageType("IE019", "CC019C", "/xsd/CC019C.xsd")
 
   /** E_POS_ACK (IE928) */
-  case object PositiveAcknowledge extends DepartureResponseMessageType("IE928", "CC928C", "/xsd/CC928C.xsd")
+  final case object PositiveAcknowledge extends DepartureResponseMessageType("IE928", "CC928C", "/xsd/CC928C.xsd")
 
   /** E_MRN_ALL (IE028) */
-  case object MrnAllocated extends DepartureResponseMessageType("IE028", "CC028C", "/xsd/CC028C.xsd")
+  final case object MrnAllocated extends DepartureResponseMessageType("IE028", "CC028C", "/xsd/CC028C.xsd")
 
   /** E_REL_TRA (IE029) */
-  case object ReleaseForTransit extends DepartureResponseMessageType("IE029", "CC029C", "/xsd/CC029C.xsd")
+  final case object ReleaseForTransit extends DepartureResponseMessageType("IE029", "CC029C", "/xsd/CC029C.xsd")
 
   /** E_WRT_NOT (IE045) */
-  case object WriteOffNotification extends DepartureResponseMessageType("IE045", "CC045C", "/xsd/CC045C.xsd")
+  final case object WriteOffNotification extends DepartureResponseMessageType("IE045", "CC045C", "/xsd/CC045C.xsd")
 
   /** E_REL_NOT (IE051) */
-  case object NoReleaseForTransit extends DepartureResponseMessageType("IE051", "CC051C", "/xsd/CC051C.xsd")
+  final case object NoReleaseForTransit extends DepartureResponseMessageType("IE051", "CC051C", "/xsd/CC051C.xsd")
 
   /** E_CTR_DEC (IE060) */
-  case object ControlDecisionNotification extends DepartureResponseMessageType("IE060", "CC060C", "/xsd/CC060C.xsd")
+  final case object ControlDecisionNotification extends DepartureResponseMessageType("IE060", "CC060C", "/xsd/CC060C.xsd")
 
   /** E_AMD_NOT (IE022) */
-  case object NotificationToAmend extends DepartureResponseMessageType("IE022", "CC022C", "/xsd/CC022C.xsd")
+  final case object NotificationToAmend extends DepartureResponseMessageType("IE022", "CC022C", "/xsd/CC022C.xsd")
 
   /** E_INC_NOT (IE182) */
-  case object IncidentNotification extends DepartureResponseMessageType("IE182", "CC182C", "/xsd/CC182C.xsd")
+  final case object IncidentNotification extends DepartureResponseMessageType("IE182", "CC182C", "/xsd/CC182C.xsd")
 
   /** E_REC_NOT (IE035) */
-  case object RecoveryNotification extends DepartureResponseMessageType("IE035", "CC035C", "/xsd/CC035C.xsd")
+  final case object RecoveryNotification extends DepartureResponseMessageType("IE035", "CC035C", "/xsd/CC035C.xsd")
 
-  case object RequestOnNonArrivedMovement extends DepartureResponseMessageType("IE140", "CC140C", "/xsd/CC140C.xsd")
+  final case object RequestOnNonArrivedMovement extends DepartureResponseMessageType("IE140", "CC140C", "/xsd/CC140C.xsd")
 
   val departureResponseValues = Set(
     AmendmentAcceptance,
@@ -167,10 +167,10 @@ object MessageType {
   // ****************
 
   /** E_REQ_REL (IE054) */
-  case object ArrivalNotification extends ArrivalRequestMessageType("IE007", "CC007C", "/xsd/CC007C.xsd")
+  final case object ArrivalNotification extends ArrivalRequestMessageType("IE007", "CC007C", "/xsd/CC007C.xsd")
 
   /** E_PRE_NOT (IE170) */
-  case object UnloadingRemarks extends ArrivalRequestMessageType("IE044", "CC044C", "/xsd/CC044C.xsd")
+  final case object UnloadingRemarks extends ArrivalRequestMessageType("IE044", "CC044C", "/xsd/CC044C.xsd")
 
   val arrivalRequestValues = Set(
     ArrivalNotification,
@@ -182,13 +182,13 @@ object MessageType {
   // ****************
 
   /** E_DES_REJ (IE057) */
-  case object DestinationOfficeRejection extends ArrivalResponseMessageType("IE057", "CC057C", "/xsd/CC057C.xsd")
+  final case object DestinationOfficeRejection extends ArrivalResponseMessageType("IE057", "CC057C", "/xsd/CC057C.xsd")
 
   /** E_GDS_REL (IE025) */
-  case object GoodsReleaseNotification extends ArrivalResponseMessageType("IE025", "CC025C", "/xsd/CC025C.xsd")
+  final case object GoodsReleaseNotification extends ArrivalResponseMessageType("IE025", "CC025C", "/xsd/CC025C.xsd")
 
   /** E_ULD_PER (IE025) */
-  case object UnloadingPermission extends ArrivalResponseMessageType("IE043", "CC043C", "/xsd/CC043C.xsd")
+  final case object UnloadingPermission extends ArrivalResponseMessageType("IE043", "CC043C", "/xsd/CC043C.xsd")
 
   val arrivalResponseValues = Set(
     DestinationOfficeRejection,
@@ -202,7 +202,7 @@ object MessageType {
   // Error Responses
   // ***************
 
-  case object XmlNack extends ErrorMessageType("IE917", "CC917C", "/xsd/CC917C.xsd")
+  final case object XmlNack extends ErrorMessageType("IE917", "CC917C", "/xsd/CC917C.xsd")
 
   val errorValues = Set(XmlNack)
 
