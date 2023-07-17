648a7b39d1e19ac104a70bb1243c04efe0bd8baa
Akila Sankar <124261162+akila-sankar@users.noreply.github.com>
2023-07-17 09:16:41 +0100

    removed commented lines

--- app/uk/gov/hmrc/transitmovements/repositories/MovementsRepository.scala
+++ app/uk/gov/hmrc/transitmovements/repositories/MovementsRepository.scala
@@ -156,7 +156,6 @@ class MovementsRepositoryImpl @Inject() (
       domainFormat = MongoFormats.movementFormat,
       indexes = Seq(
         IndexModel(Indexes.ascending("updated"), IndexOptions().expireAfter(appConfig.documentTtl, TimeUnit.SECONDS)),
-        //IndexModel(Indexes.ascending(fieldNames = "localReferenceNumber")),
         IndexModel(Indexes.ascending(fieldNames = "localReferenceNumber", "messageSender")),
         IndexModel(Indexes.ascending("movementReferenceNumber"), IndexOptions().background(true))
       ),
