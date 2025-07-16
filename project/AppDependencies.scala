13aa1c4155e592ba16951ab4e1c845cf426cbb2f
Arei Mohammed <arei.mohammed@digital.hmrc.gov.uk>
2025-07-16 13:59:28 +0100

    test

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ object AppDependencies {
 
   private val catsVersion          = "2.13.0"
   private val hmrcMongoVersion     = "2.6.0"
-  private val hmrcBootstrapVersion = "9.13.0"
+  private val hmrcBootstrapVersion = "9.16.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"    % hmrcBootstrapVersion,
